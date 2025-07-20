59f3a2b3bb39355c05a3a9df0a51a5a9a026bcd1
KEVIN-117 <krodriguez17@gmail.com>
2025-07-20 15:55:29 -0400

    Refactor code structure for improved readability and maintainability

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const connectDB = require('./server/database/connection');
 
 const app = express();
 
-dotenv.config( { path : 'config.env'} )
-const PORT = process.env.PORT || 8080
+dotenv.config()
+const PORT = process.env.PORT
 
 // log requests
 app.use(morgan('tiny'));
